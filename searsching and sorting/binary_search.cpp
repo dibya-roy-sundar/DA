e3e59ca945565b5bd3cf146ab79158c2dd92ca4c
Dibya Sundar Roy <dibya.roy.12345@gmail.com>
2023-08-26 20:28:29 +0530

    hsf

--- searsching and sorting/binary_search.cpp
+++ searsching and sorting/binary_search.cpp
@@ -28,12 +28,73 @@ int binary( int arr[], int size , int key){
     }
 }
 
+int leftmost(int arr[], int size , int key){
+    int s=0;
+    int e=size-1;
+    int mid=s+(e-s)/2;
+    int i=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            i=mid;
+            e=mid-1;
+        }else if(key>arr[mid]){
+            s=mid+1;
+        }else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return i;
+}
+
+int rightmost(int arr[], int size , int key){
+    int s=0;
+    int e=size-1;
+    int mid=s+(e-s)/2;
+    int i=-1;
+    while(s<=e){
+        if(arr[mid]==key){
+            i=mid;
+            s=mid+1;
+        }else if(key>arr[mid]){
+            s=mid+1;
+        }else{
+            e=mid-1;
+        }
+        mid=s+(e-s)/2;
+        // cout<<i<<endl;
+    }
+        return i;
+}
+
+// find peak index of a mountain array
+int  peak(int arr[],int n){
+    int s=0;
+    int e=n-1;
+    int mid=s+(e-s)/2;
+    while(s<e){
+        // for avoid repetation of iteration we should use s<e instead of s<=e
+        if((arr[mid]<arr[mid+1])){
+            s=mid+1;
+        }else{
+            e=mid;
+        }
+        mid=s+(e-s)/2;
+    }
+    return s;
+}
+
+
 int main(){
-    int odd[5]={1,34,45,67,89};
-    int even[6]={2,23,45,46,88,94};
-    int oddindex=binary(odd,5,45);
-    int evenindex=binary(even,6,34);
-    cout<<oddindex<<endl<<evenindex;
+    int odd[11]={0,1,1,2,2,2,2,3,3,3,3};
+    int even[6]={1,3,5,6,7,1};
+    // int oddindex=binary(odd,5,45);
+    // int evenindex=binary(even,6,34);
+    // cout<<oddindex<<endl<<evenindex;
+    // cout<<leftmost(odd,11,3)<<endl;
+    // cout<<rightmost(odd,11,3)<<endl;
+    // cout<<"total no of occurance is   " <<(rightmost(odd,11,3)-leftmost(odd,11,3))+1<<endl;
+    cout<<peak(even,6)<<endl;
     return 0;
 
 }
