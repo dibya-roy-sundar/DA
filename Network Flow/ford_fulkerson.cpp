efbd06fc34cae6c463da6364fad133bba285753f
Dibya Sunadr Roy <dibya.roy.12345@gmail.com>
2024-04-01 19:48:39 +0530

    network flow using dfs

--- Network Flow/ford_fulkerson.cpp
+++ Network Flow/ford_fulkerson.cpp
@@ -1,10 +1,10 @@
-# include <iostream>
+#include <iostream>
 #include<cstring>
 #include <queue>
 using namespace std;
 int V;
 
-int bfs(vector<vector<int>> rgraph,int s , int t,int parent[]){
+int bfs(vector<vector<int>>& rgraph,int s , int t,int parent[]){
     queue<int> q;
     int vis[V]={0};
     q.push(s);
@@ -36,6 +36,32 @@ int bfs(vector<vector<int>> rgraph,int s , int t,int parent[]){
 
 }
 
+int dfs(vector<vector<int>>& graph,int t,int parent[],int node,vector<int>& vis){
+    
+
+    if(node==t) return true;
+
+    vis[node]=1;
+
+    for(int i=0;i<V;i++){
+        if( !vis[i] && graph[node][i]>0){
+            parent[i]=node;
+           if(dfs(graph,t,parent,i,vis)){
+            // reset vis array for next round;
+            for(int i=0;i<V;i++){
+                vis[i]=0;
+            }
+            return true;
+           } 
+           vis[i]=0;
+           parent[i]=-1;
+        }
+    }
+
+    return false;
+
+}
+
 int fordFulkerson(vector<vector<int>> graph,int s , int t){
     // we have to secure original data , we have to form a residual graph from original data
         vector<vector<int>> rgraph(V,vector<int> (V));
@@ -55,6 +81,9 @@ int fordFulkerson(vector<vector<int>> graph,int s , int t){
     // to store the augmented path we will use parent array 
     int parent[V];
     // now after bfs we store the augmented path into parent array
+
+    //  vector<int> vis(V,0);  (for dfs)
+    //  while(dfs(graph,t,parent,s,vis))    (for dfs)
     while(bfs(rgraph,s,t,parent)){
         // if true , we find an augmented path
     //  2.find  bottleneck throughout the augmented path
@@ -84,7 +113,14 @@ int fordFulkerson(vector<vector<int>> graph,int s , int t){
     // for every iteration , this is confirm that the augmented path will be different because , after minus bottleneck from capacity 
     // any of the edge's capacity will be 0 from the above path , in next iteration , that node wouldn't be pushed into queue and wouldn't be
     //  a part of our new augmented path
+
+
+    // reset parent array for next dfs 
+    // for(int i=0;i<V;i++){
+    //         parent[i]=-1;
+    //     }
     }
+
     return maxflow; 
    
 }
